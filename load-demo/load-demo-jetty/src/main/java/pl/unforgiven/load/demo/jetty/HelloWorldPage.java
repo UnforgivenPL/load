9144a6b3643061d8c2005781f175f9ad422ad20b
Miki <sorrow@unforgiven.pl>
2021-02-24 17:44:36 +0200

    1 hello jetty (#4)
    #1 rough proof of concept that seems to work
    
    plus some graphics resources for later

---
+++ load-demo/load-demo-jetty/src/main/java/pl/unforgiven/load/demo/jetty/HelloWorldPage.java
@@ -0,0 +1,19 @@
+package pl.unforgiven.load.demo.jetty;
+
+import pl.unforgiven.load.core.LoadPage;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A page displaying a simple "hello, world" for jetty.
+ * @author miki
+ * @since 2021-02-14
+ */
+public class HelloWorldPage implements LoadPage {
+
+  @Override
+  public Object get(List<String> path, Map<String, String[]> parameters) {
+    return "hello, dear world; request path was "+ String.join(", ", path);
+  }
+}
