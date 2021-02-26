e4123eb835a1336e56696d7a186d70bca4a9b52d
miki <sorrow@unforgiven.pl>
2021-02-26 23:45:00 +0200

    #5 path matching implemented (somewhat)
    even some unit tests

---
+++ load-core/src/test/java/pl/unforgiven/load/core/path/PathMatcherTest.java
@@ -0,0 +1,66 @@
+package pl.unforgiven.load.core.path;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class PathMatcherTest {
+
+  private final PathMatcher matcher = new PathMatcher();
+  private final Map<String, List<String>> parameters = new HashMap<>();
+
+  @BeforeEach
+  void setup() {
+    this.parameters.clear();
+  }
+
+  private void assertOneParameter(String parameter, String... value) {
+    Assertions.assertEquals(1, this.parameters.size(), "these are the parameters: "+String.join(", ", this.parameters.keySet()));
+    Assertions.assertTrue(this.parameters.containsKey(parameter), "["+parameter+"] not found in parameter map: "+ String.join(", ", this.parameters.keySet()));
+    Assertions.assertEquals(Arrays.asList(value), this.parameters.get(parameter));
+  }
+
+  @Test
+  void testNoParameters() {
+    Assertions.assertTrue(matcher.isMatching("test", "test", this.parameters));
+    Assertions.assertTrue(matcher.isMatching("", "", this.parameters));
+    Assertions.assertTrue(this.parameters.isEmpty());
+
+    Assertions.assertFalse(matcher.isMatching("test", "fail", this.parameters));
+    Assertions.assertFalse(matcher.isMatching("test", "", this.parameters));
+    Assertions.assertFalse(matcher.isMatching("", "fail", this.parameters));
+    Assertions.assertTrue(this.parameters.isEmpty());
+  }
+
+  @Test
+  void testWholeParameterNoRegexp() {
+    Assertions.assertTrue(matcher.isMatching("{v}", "test", this.parameters));
+    this.assertOneParameter("v", "test");
+  }
+
+  @Test
+  void testPrePostFixedParameterNoRegexp() {
+    Assertions.assertTrue(matcher.isMatching("some{var}", "something", this.parameters));
+    this.assertOneParameter("var", "thing");
+
+    Assertions.assertTrue(matcher.isMatching("{var}thing", "something", this.parameters));
+    this.assertOneParameter("var", "thing", "some");
+
+    Assertions.assertTrue(matcher.isMatching("so{var}ing", "something", this.parameters));
+    this.assertOneParameter("var", "thing", "some", "meth");
+  }
+
+  @Test
+  void testWholeParameterRegexp() {
+    Assertions.assertTrue(matcher.isMatching("{num:\\d+}", "42", this.parameters));
+    this.assertOneParameter("num", "42");
+
+    Assertions.assertFalse(matcher.isMatching("{num:\\d+}", "42a", this.parameters));
+  }
+
+}
